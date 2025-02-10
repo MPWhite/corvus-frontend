90831adbe6d55ff42b983d053135e46ff8091f9c
Matt White <matthewpaul.white@airtable.com>
2025-02-10 11:23:41 -0500

    Fix the deploy stuff that was overwritten

--- server.cjs
+++ server.cjs
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const API_URL = 'https://corvus-be-ea11e5b5e66c.herokuapp.com';
+
 // Serve static files
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+      target: 'https://corvus-be-ea11e5b5e66c.herokuapp.com/api',
+      changeOrigin: true,
+  })
+);
+
 
 // Handle client-side routing
 app.get('*', (req, res) => {
